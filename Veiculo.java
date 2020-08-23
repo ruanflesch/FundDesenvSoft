f91a3b93729b09552f79849e2d0890a142ffbe10
gustavokunde <gustavokunde@gmail.com>
2020-08-23 17:50:04 -0300

    adicionando métodos da interface veiculo

--- Veiculo.java
+++ Veiculo.java
@@ -1,3 +1,17 @@
 public interface Veiculo {
     
+    public int getCodigo();
+
+    public String getCor();
+
+    public String getMarca();
+
+    public double getValor();
+
+    public void setCor();
+
+    public void setMarca();
+
+    public void setValor();
+
 }
