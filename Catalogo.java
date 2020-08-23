12b65dab2e11b80652d538f0dfcc65d215f1f826
gustavokunde <gustavokunde@gmail.com>
2020-08-23 17:58:38 -0300

    implementação basica da classe catalogo

--- Catalogo.java
+++ Catalogo.java
@@ -1,3 +1,13 @@
+import java.util.List;
+
 public class Catalogo{
-    
+    private List<Veiculo> veiculos;
+
+    public Catalogo(){
+        veiculos = new ArrayList<>();
+    }
+
+    public List<Veiculo> getVeiculos(){
+        return veiculos;
+    }
 }
